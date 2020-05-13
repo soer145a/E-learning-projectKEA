3735ae2db3cb2d17d2991d130428badb7d41419c
Mikkel Madsen <mikk837k@stud.kea.dk>
2020-05-13 09:47:28 +0200

    Banner has been styled, Error on on index page has been fixed when no user is signed in, active class has been removed from header when on the profile page, sign up and login button has been added to my progress div when user isn't logged in, the users name has been added to the banner when logged in, hover effect added to nav items

--- E-learning_Project/index.php
+++ E-learning_Project/index.php
@@ -2,10 +2,13 @@
 session_start();
 include_once('DB_Connect/connection.php');
 include_once('DB_Connect/procedures.php');
-$getUserProgress = str_replace("::uID::",$_SESSION['userID'],$getUserProgress);
-$sql = $getUserProgress;
-$result = $conn->query($sql);
-$aProgressArray = [];
+
+if(isset($_SESSION['loginStatus'])) {
+  $sName = $_SESSION['firstName'];
+  $getUserProgress = str_replace("::uID::",$_SESSION['userID'],$getUserProgress);
+  $sql = $getUserProgress;
+  $result = $conn->query($sql);
+  $aProgressArray = [];
   if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc() ) {
@@ -17,6 +20,7 @@ $aProgressArray = [];
   } else {
           echo "0 results";
   }
+}
 $conn->close();
 include_once('components/compTop.php');
 ?>
@@ -25,15 +29,24 @@ include_once('components/compTop.php');
       <img src="assets/banner_element1.svg" alt="banner element">
       <img src="assets/banner_element2.svg" alt="banner element">
       <img src="assets/banner_element3.svg" alt="banner element">
-      <h1>Hello - Ready to learn about Relational Databases? Jump right in</h1>
-      <a href="course.php" data-navtag="course" onclick="setSessionData(this)"><button class="btn-secondary">BEGIN COURSE</button></a>
+      <div id="bannerTxt">
+        <?php 
+          if(isset($_SESSION["loginStatus"])) {
+            echo "<h1>Hi ".$sName." - Ready to learn about Relational Databases? Jump right in</h1>";
+          } else {
+            echo "<h1>Hello - Ready to learn about Relational Databases? Jump right in</h1>";
+          }
+        ?>
+        <a href="course.php" data-navtag="course" onclick="setSessionData(this)"><button class="btn-secondary">BEGIN COURSE</button></a>
+      </div>
+        
     </div>
     <span id="background">
       <img src="assets/Polygon 1.svg" alt="" />
       <img src="assets/Polygon 2.svg" alt="" />
     </span>
     <main id="indexPage">
-      <section class="sectionwrapper">
+      <section id="learn"class="sectionwrapper">
         <h2>LEARN</h2>
         <div class="container">
           <h3>Relational Databases</h3>
@@ -46,30 +59,39 @@ include_once('components/compTop.php');
           <a href="course.php" data-navtag="course" onclick="setSessionData(this)">BEGIN COURSE</a>
         </div>
       </section>
-      <section class="sectionwrapper">
+      <section id="my_progress" class="sectionwrapper">
         <h2>MY PROGRESS</h2>
         <div class="container">
+          <?php 
+           if(!isset($_SESSION['loginStatus'])) {
+            echo "<div class='login_signup'>
+                    <a href='signup.php' data-navtag='index' onclick='setSessionData(this)'><button class='btn-primary'>SIGN UP</button></a>
+                    <h3>OR</h3>
+                    <a href='login.php' data-navtag='index' onclick='setSessionData(this)'><button class='btn-tertiary'>LOG IN</button></a>
+                  </div>";
+           }
+          ?>
         <div id="totalProgress">
         </div>
         <div id="individualTopicProgress">
-        
-          <?php 
-              foreach ($aProgressArray as $obj){
-                $courseName = $obj->courseName;
-                $progress = $obj->status;
-                $percentages = $progress * 100 / 4;
-                echo "<div class='iProgressItem'>
-                <p>$courseName</p>
-                <svg height='20' width='20' viewBox='0 0 20 20'>
-          <circle r='10' cx='10' cy='10' fill='var(--color-five)'/>
-          <circle r='5' cx='10' cy='10' fill='transparent' stroke='var(--color-six)' stroke-dasharray='calc($percentages * 31.42 / 100) 31.42' stroke-width='10'
-          transform='rotate(-90) translate(-20)'/>
-          <circle r='8' cx='10' cy='10' fill='white'/>
-          <text x='5' y='11.5' class='svgText'>$progress / 4</text>
-        </svg>
-                
-                </div>";
-              }     
+          <?php
+            if(isset($_SESSION['loginStatus'])) {
+                foreach ($aProgressArray as $obj){
+                  $courseName = $obj->courseName;
+                  $progress = $obj->status;
+                  $percentages = $progress * 100 / 4;
+                  echo "<div class='iProgressItem'>
+                          <p>$courseName</p>
+                          <svg height='20' width='20' viewBox='0 0 20 20'>
+                            <circle r='10' cx='10' cy='10' fill='var(--color-five)'/>
+                            <circle r='5' cx='10' cy='10' fill='transparent' stroke='var(--color-six)' stroke-dasharray='calc($percentages * 31.42 / 100) 31.42' stroke-width='10'
+                            transform='rotate(-90) translate(-20)'/>
+                            <circle r='8' cx='10' cy='10' fill='white'/>
+                            <text x='5' y='11.5' class='svgText'>$progress / 4</text>
+                          </svg>
+                        </div>";
+                }
+              };
           ?>
         </div>
         
