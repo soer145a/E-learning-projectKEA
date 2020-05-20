93fc13c183d1b017d5be1a875a59768a5c63c879
Lev-pt <daniel-beck@hotmail.com>
2020-05-20 10:52:52 +0200

    bb

--- E-learning_Project/components/compTop.php
+++ E-learning_Project/components/compTop.php
@@ -24,7 +24,8 @@
   <link rel="stylesheet" href="signup.css" /> <!-- 04/05/20 - 12.35 - Daniel har tilføjet ref til signup.css -->
   <link rel="stylesheet" href="login.css" /> <!-- 04/05/20 - 12.35 - Daniel har tilføjet ref til login.css -->
   <link rel="stylesheet" href="glossary.css" /> <!-- 06/05/20 - 15.35 - Daniel har tilføjet ref til syllabus.css -->
-  <link rel="stylesheet" href="admin.css" /> <!-- 19/05/20 - 19.09 - Mikkek har tilføjet ref til admin.css -->
+  <link rel="stylesheet" href="admin.css" /> <!-- 19/05/20 - 19.09 - Mikkel har tilføjet ref til admin.css -->
+  <link rel="stylesheet" href="profile.css" /> <!-- 20/05/20 - 19.09 - Daniel har tilføjet ref til profile.css -->
 </head>
 
 <body>
